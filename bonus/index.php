f44ddc0284cd083865198e5aae8d1b95979d571c
Nino Chimento <saninoc84@gmail.com>
2020-02-24 16:41:55 +0100

    header css

--- bonus/index.php
+++ bonus/index.php
@@ -11,6 +11,13 @@
         <img src="../img/download.png" alt="">
         <h2>Privacy e termini</h2>
       </a>
+      <ul>
+        <li><a href="#">Introduzione</a> </li>
+        <li><a href="#"> Norme sulla privacy</a> </li>
+        <li><a href="#"> termini di servizio</a> </li>
+        <li><a href="#"> tecnologie</a> </li>
+        <li><a href="#"> domande frequenti</a> </li>
+      </ul>
     </header>
   </body>
 </html>
